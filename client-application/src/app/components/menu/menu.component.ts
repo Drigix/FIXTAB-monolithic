e9d163e56c2bbac284840723380489f334657f77
Michał Ławinski <michrarr@gmail.com>
2023-03-25 17:58:30 +0100

    Change menu component

--- client-application/src/app/components/menu/menu.component.ts
+++ client-application/src/app/components/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IMenuItem } from 'src/app/entitites/menu-item.model';
 import { MenuService } from './menu.service';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'fixtab-menu',
