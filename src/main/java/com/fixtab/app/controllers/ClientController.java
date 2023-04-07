a1987b12c8cf09fabfffe48ea0e134a9a070a582
Michał Ławinski <michrarr@gmail.com>
2023-04-07 23:56:23 +0200

    Add client request(get, post, delete)

---
+++ src/main/java/com/fixtab/app/controllers/ClientController.java
@@ -0,0 +1,44 @@
+package com.fixtab.app.controllers;
+
+import com.fixtab.app.models.db.employees.EmployeeRoleModel;
+import com.fixtab.app.models.requests.ClientRequest;
+import com.fixtab.app.models.responses.ClientResponse;
+import com.fixtab.app.services.interfaces.ClientService;
+import com.fixtab.app.services.interfaces.EmployeeRoleService;
+import com.sun.net.httpserver.Authenticator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/client")
+public class ClientController {
+
+    private final ClientService clientService;
+
+    @GetMapping("getAllClients")
+    public List<ClientResponse> getAllClients() {
+        return clientService.getAllClients();
+    }
+
+    @GetMapping("getAllNotDeletedClients")
+    public List<ClientResponse> getAllNotDeletedClients() {
+        return clientService.getAllNotDeletedClients();
+    }
+
+    @PostMapping("createClient")
+    public ResponseEntity<?> createClient(@RequestBody ClientRequest clientRequest) {
+        clientService.createClient(clientRequest);
+        return new ResponseEntity<Authenticator.Success>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("deleteClient/{clientId}")
+    public ResponseEntity<HttpStatus> deleteClient(@PathVariable Integer clientId) {
+        clientService.deleteClient(clientId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
