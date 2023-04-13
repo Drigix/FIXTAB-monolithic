24e22e65780f5f786e281bf6bf83cebf8f0ce761
Michał Ławinski <michrarr@gmail.com>
2023-04-13 22:10:47 +0200

    Create class to exceptions in application

--- src/main/java/com/fixtab/app/AppApplication.java
+++ src/main/java/com/fixtab/app/AppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
